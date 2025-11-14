aee77fea52425f32ff21c577f3f01a7011716442
LucasnProg <lucasnprog@gmail.com>
2025-11-14 14:34:19 -0300

    Versão em desenvolvimento do projeto

--- backend/app.py
+++ backend/app.py
@@ -1,28 +1,67 @@
-from fastapi import FastApi, UploadFile, File, HTTPException
-from fastapi.responses import StreamingResponses
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 from PIL import Image
 import io
-from transformers import pipeline 
 import torch
+from realesrgan import RealESRGAN
+import numpy as np
+import cv2
 
-app = FastApi()
+MODEL_PATH = 'backend\models\RealESRGAN_x4plus.pth'
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+try:
+    model = RealESRGAN(scale=4, model_path=MODEL_PATH).to(DEVICE)
+    model.load_weights(MODEL_PATH)
+    print(f"Modelo carregado com sucesso no {DEVICE}")
+except Exception as exc:
+    print("Não foi possivel carregar o modelo")
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
+
+app.add_middleware(
+   CORSMiddleware,
+   allow_origins = origins,
+   allow_credentials = True,
+   allow_methods= ["*"],
+   allow_headers= ["*"] 
+)
+ 
 @app.post("/upscale/")
 async def upscale_image(file: UploadFile = File(...)):
     try:
         contents = await file.read()
         image = Image.open(io.BytesIO(contents)).convert("RGB")
 
-        # TODO: Substituir por chamada ao modelo de I.A. real
-        # Exemplo de mock: simplesmente redimensiona a imagem para o dobro (não é super-resolução real, mas simula o output)
-        width, height = image.size
-        upscaled_image = image.resize((width * 2, height * 2), Image.LANCZOS)
-        # upscaled_image = model.upscale(image) # Exemplo com RealESRGAN
+        if model:
+            img_np = np.array(image)
+            img_np_rgb = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
+
+            upscaled_image_np = model.predict(img_np_rgb)
+            upscaled_image = Image.fromarray(cv2.cvtColor(upscaled_image_np, cv2.COLOR_BGR2RGB)) 
+        else:
+            width, height = image.size
+            upscaled_image = image.resize((width * 2, height * 2), Image.LANCZOS)
+            print("Usando mock de upscale, pois o modelo real não foi carregado.")
 
         img_byte_arr = io.BytesIO()
-        upscaled_image.save(img_byte_arr, format=file.filename.split(".")[-1] if "." in file.filename else "PNG")
+        output_format_save = file.filename.split(".")[-1].upper() if "." in file.filename else "PNG"
+
+        if output_format_save not in ["JPEG", "PNG", "WEBP", "BMP"]:
+            output_format_save = "PNG"
+
+        upscaled_image.save(img_byte_arr, format=output_format_save)
         img_byte_arr.seek(0)
 
-        return StreamingResponses(img_byte_arr, media_type=file.content_type)
+        media_type = file.content_type if file.content_type in ["image/jpeg", "image/png"] else "image/png"
+
+        return StreamingResponse(img_byte_arr, media_type=file.content_type)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,82 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { ReactCompareImage } from 'react-compare-image';
 import './App.css'
+import { upScaleImage } from './services/api';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [originalImage, setOriginalImage] = useState(null);
+  const [upscaleImage, setUpscaleImage] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
+
+  const onDropImage = useCallback(
+    async (accepteFiles) => {
+      const file = accepteFiles[0];
+      if(!file) return;
+
+      setOriginalImage(URL.createObjectURL(file));
+      setOriginalImage(null);
+      setError(null);
+      setLoading(true);
+
+      const formData = new formData();
+      formData.append('file', file);
+
+      try{
+        const response = upScaleImage(formData);
+
+        if (!response.ok) {
+          const errorData = await response.json();
+          throw new Error(errorData.detail || 'Erro ao fazer o upload do arquivo');
+        }
+
+        const blob = response.blob();
+
+        setUpscaleImage(URL.createObjectURL(blob));
+      } catch (error) {
+        console.log('erro no upload da imagem: ',error);
+        setError(error.mensage || 'falha ao processar imagem');
+      } finally{
+        setLoading(false);
+      }
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropZone({ onDrop, accept: { 'image/*': ['.jpeg', '.png', '.jpg'] } });
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+            <h1>Melhore a resolução da sua imagem</h1>
+
+            <div {...getRootProps()} className={`dropzone ${isDragActive ? 'active' : ''}`}>
+                <input {...getInputProps()} />
+                {
+                    isDragActive ?
+                    <p>Solte a imagem aqui...</p> :
+                    <p>Arraste e solte uma imagem aqui, ou clique para selecionar</p>
+                }
+            </div>
+
+            {loading && <p>Processando imagem...</p>}
+            {error && <p className="error">{error}</p>}
+
+            {(originalImage && upscaledImage) && (
+                <div className="image-comparison-container">
+                    <h2>Resultado</h2>
+                    <ReactCompareImage
+                        itemOne={<img src={originalImage} alt="Original" />}
+                        itemTwo={<img src={upscaledImage} alt="Upscaled" />}
+                        keyboardIncrement="1%"
+                        position={50}
+                        className="comparison-slider"
+                    />
+                    <button onClick={handleDownload} className="download-button">Baixar Imagem Melhorada</button>
+                </div>
+            )}
+             {!originalImage && <p>Faça o upload de uma imagem para começar!</p>}
+        </div>
   )
 }
 
